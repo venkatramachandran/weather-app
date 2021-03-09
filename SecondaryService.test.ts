9101f391eee3335b9770babca8646e4f075dc14f
Venkat Ramachandran <venkyramachandran82@gmail.com>
2021-03-09 21:23:12 +0000

    updated tests

--- SecondaryService.test.ts
+++ SecondaryService.test.ts
@@ -1,5 +1,45 @@
+import fetch from 'node-fetch';
+import { mocked } from 'ts-jest/utils';
+import SecondaryService from './SecondaryService';
+
+jest.mock('node-fetch', () => {
+    return jest.fn();
+});
+
 describe('SecondaryService', () => {
-    it('', () => {
-        expect(1).toBe(1);
+    let s: SecondaryService;
+    beforeEach(() => {
+        s = new SecondaryService('http://secondary.host', 'apiKey');
+        mocked(fetch).mockClear();
+    })
+    it('returns data when api invocation is successful', async () => {
+        mocked(fetch).mockImplementation((): Promise<any> => {
+            return Promise.resolve({
+              json() {
+                return Promise.resolve({
+                    main: {
+                        temp: -1
+                    },
+                    wind: {
+                        speed: -1
+                    }
+                });
+              }
+            });
+          });
+        const w = await s.getWeather('Test');
+        expect(mocked(fetch).mock.calls[0]?.[0]).toBe('http://secondary.host?appid=apiKey&q=Test&units=metric&language=en');
+        expect(w).toStrictEqual({
+            temperature_degrees: -1,
+            wind_speed: -3.6
+        })
+    });
+    it('throws when api invocation is unsuccessful', async () => {
+        mocked(fetch).mockImplementation((): Promise<any> => {
+            return Promise.reject(new Error('Simple'));
+        });
+
+        expect(() => s.getWeather('Test')).rejects.toThrowError('Simple');
+        expect(mocked(fetch).mock.calls[0]?.[0]).toBe('http://secondary.host?appid=apiKey&q=Test&units=metric&language=en');
     });
 });
