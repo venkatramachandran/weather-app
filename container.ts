9c5110b3c769cb30ec969eda38dcb7fa2f66fe39
Venkat Ramachandran <venkyramachandran82@gmail.com>
2021-03-10 06:05:08 +0000

    simplified config extraction

--- container.ts
+++ container.ts
@@ -4,7 +4,6 @@ import {container} from 'tsyringe';
 
 const config = dotenv.config();
 
-const configKeys = ['primary_api_url', 'primary_api_key', 'secondary_api_url', 'secondary_api_key', 'cache_time_millis', 'port', 'app_name', 'log_level'];
-configKeys.forEach((k: string) => {
+Object.keys(config.parsed!).forEach((k: string) => {
     container.register(k, {useValue: config.parsed![k]});
 });
